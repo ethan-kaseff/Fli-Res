fa64687514b143624eb65ead10bed93c5cea1a47
Ethan Kaseff <ethan@kaseff.com>
2021-05-20 18:33:00 -0500

    start with react and npm install

--- backend/routes/api/index.js
+++ backend/routes/api/index.js
@@ -2,6 +2,10 @@ const router = require('express').Router();
 const sessionRouter = require('./session.js');
 const usersRouter = require('./users.js');
 
+router.post('/test', function (req, res) {
+    res.json({ requestBody: req.body });
+});
+
 router.use('/session', sessionRouter);
 
 router.use('/users', usersRouter);
@@ -16,9 +20,6 @@ module.exports = router;
 
 // For Testing
 
-// router.post('/test', function (req, res) {
-//     res.json({ requestBody: req.body });
-// });
 
 // // GET /api/set-token-cookie
 // const asyncHandler = require('express-async-handler');
