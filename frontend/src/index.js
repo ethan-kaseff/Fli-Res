fa04f44c129bb85eee75ce3418949d20b21fbe4b
Ethan Kaseff <ethan@kaseff.com>
2021-09-21 15:39:24 -0400

    modal working but unstyled, and Login "button" broken

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -2,6 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import { Provider } from 'react-redux';
 import { BrowserRouter } from 'react-router-dom';
+import { ModalProvider } from './context/Modal';
 
 import './index.css';
 import App from './App';
@@ -22,9 +23,11 @@ if (process.env.NODE_ENV !== 'production') {
 function Root() {
   return (
     <Provider store={store}>
-      <BrowserRouter>
-        <App />
-      </BrowserRouter>
+      <ModalProvider>
+        <BrowserRouter>
+          <App />
+        </BrowserRouter>
+      </ModalProvider>
     </Provider>
   );
 }
