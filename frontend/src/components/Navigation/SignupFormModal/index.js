f3e73bb06d7becdb54b386e1c3efbb64dcce8d8a
Ethan Kaseff <ethan@kaseff.com>
2021-09-28 16:32:06 +0100

    rand into weird modal issues

--- frontend/src/components/Navigation/SignupFormModal/index.js
+++ frontend/src/components/Navigation/SignupFormModal/index.js
@@ -1,5 +1,5 @@
 import React, { useState } from 'react';
-import { Modal } from '../../../context/Modal';
+import { Modal } from '../../../context/modal.js';
 import SignupForm from './SignupForm';
 
 function SignupFormModal() {
