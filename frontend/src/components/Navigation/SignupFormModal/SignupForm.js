840c8313abd57c9a33e30e67513a8c533e6eebb0
Ethan Kaseff <ethan@kaseff.com>
2021-09-30 17:26:50 +0100

    modal input update

--- frontend/src/components/Navigation/SignupFormModal/SignupForm.js
+++ frontend/src/components/Navigation/SignupFormModal/SignupForm.js
@@ -3,8 +3,6 @@ import * as sessionActions from '../../../store/session';
 import { useDispatch, useSelector } from 'react-redux';
 import { Redirect } from 'react-router-dom';
 
-import './SignupForm.css';
-
 function SignupForm() {
   const dispatch = useDispatch();
   const sessionUser = useSelector(state => state.session.user);
